62ea3ed8ab1c5642afe2386e633debd31cd360f4
visualstudio <visualstudio@230-PC02>
2019-02-25 15:31:20 -0500

    objects

--- c0739297-week7/js/main.js
+++ c0739297-week7/js/main.js
@@ -1,1 +1,51 @@
 ﻿// Your code here!
+function p() {
+    sum = 0;
+    counter = 117;
+    while (counter >= 11) {
+        sum += counter;
+        counter -= 8;
+    }
+    return sum;
+}
+document.write("sum is ", p());
+
+
+
+var bird = {
+    genus: "corvus",
+    species: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "Nervermore",
+    maxOffSpring: 5,
+    noisy: true,
+    deadly: false
+};
+
+
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffSpring: 3,
+    noisy: true,
+    deadly: true
+};
+
+
+var car = {
+    color: "Red",
+    numtires: 4,
+    model: "honda"
+};
+
+
+
+var motorcycle = {
+    color: "blue",
+    numtires: 2,
+    model: "harley"
+};
